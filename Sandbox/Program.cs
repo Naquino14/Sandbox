62c66dd3f2e6ed122e1853c188377eee25e705ca
Nathaniel Aquino <naquino14@outlook.com>
2022-01-14 11:38:03 -0500

    migrate arc

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using c = System.Console;
 using mybsallsa;
-using ARC;
 using GenericLinkedList;
 using System.Security.Cryptography;
 using System.Security;
@@ -14,26 +13,7 @@ namespace Sandbox
     {
         public static void Main(string[] vs)
         {
-            //using (Aes aes = Aes.Create())
-            //{
-            //    aes.Key = Encoding.ASCII.GetBytes("funnie key moment");
-            //    // nah this is inconvenient to use
-            //}
-
-            var arc = new ARC128();
-            c.Write("IV: ");
-            var iv = arc.GenerateIV();
-            foreach (var x in iv)
-                c.Write(x.ToString("X"));
-            c.WriteLine($" Size: {iv.Length} bytes");
-
-            c.Write(" K: ");
-            var key = arc.GenerateKey();
-            foreach (var y in key)
-                c.Write(y.ToString("X"));
-            c.WriteLine($" Size: {key.Length} bytes");
-
-            arc.Encrypt("This is random data loleyufucbgfkuyvjsbgdfuikxkhfvbaskiuzghszdkfighawoieuskjghrfpiWUGEPI7FGwyWP9YUERP98SYGPIAHYEPSRDGYUAPEIHY", key, iv);
+            
         }
     }
 }
